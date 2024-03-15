2019c0e2d09d3fd17a1d9fd1798e24131106b924
puenteformacio <101072340+puenteformacion@users.noreply.github.com>
2024-03-15 07:04:48 +0100

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+las info recibida es:
+<br>
+<?php
+echo "el nombre es:".$_POST["nombre"];
+?>
+</body>
+</html>
